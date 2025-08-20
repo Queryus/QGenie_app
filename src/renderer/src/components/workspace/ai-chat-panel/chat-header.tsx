07332a6b832f1bf8e3543bf2d91b557a2d2d5bc6
나형진 <skgudwls@konkuk.ac.kr>
2025-08-20 13:44:27 +0900

    feat: add chat history tap

--- src/renderer/src/components/workspace/ai-chat-panel/chat-header.tsx
+++ src/renderer/src/components/workspace/ai-chat-panel/chat-header.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import { BotMessageSquare, History } from 'lucide-react'
 import SearchBar from './search-bar'
+import ChatHistoryPanel from './chat-history-panel'
 
 interface ChatHeaderProps {
   onSearchChange: (term: string) => void
@@ -12,6 +14,12 @@ interface ChatHeaderProps {
  * @returns JSX.Element
  */
 export default function ChatHeader({ onSearchChange }: ChatHeaderProps): React.JSX.Element {
+  const [isHistoryOpen, setIsHistoryOpen] = useState(false)
+
+  const toggleHistory = (): void => {
+    setIsHistoryOpen((prev) => !prev)
+  }
+
   return (
     <div className="self-stretch pl-4 pr-3 py-3 bg-neutral-800 border-b border-neutral-700 inline-flex justify-between items-center">
       <div className="flex justify-start items-center gap-2">
@@ -22,7 +30,12 @@ export default function ChatHeader({ onSearchChange }: ChatHeaderProps): React.J
       </div>
       <div className="flex justify-start items-center gap-4">
         <SearchBar onSearchChange={onSearchChange} />
-        <History className="size-4 stroke-[#E4E4E4]" />
+        <div className="relative flex">
+          <button onClick={toggleHistory} className="focus:outline-none">
+            <History className="size-4 stroke-[#E4E4E4] cursor-pointer" />
+          </button>
+          {isHistoryOpen && <ChatHistoryPanel />}
+        </div>
       </div>
     </div>
   )
